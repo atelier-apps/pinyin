9c6295c7fcd5ba6c84ab27ca30a8b38a70733bba
吉永稔弘 <yoshinaga@Toshys-MacBook.local>
2019-08-18 06:44:13 +0900

    fix translate

--- pinyin/src/components/ProtoScreen.vue
+++ pinyin/src/components/ProtoScreen.vue
@@ -3,11 +3,12 @@
   <h1>{{app_name}}</h1>
   <div>
     <p> {{headerMsg}}</p>
-    <input v-on:input="hoge" v-bind:class="{ 'error-input' : isError}" v-model="target">
+    <input v-on:input="translate" v-bind:class="{ 'error-input' : isError}" v-model="target">
     <p v-if="isError" class="error-text"> {{errorMsg}}</p>
+    <p v-if="isOverLimit" class="error-text"> {{lengthErrorMsg}}</p>
   </div>
-  <ResultArea title="中国語読み" v-bind:responce="responce['zh']" />
-  <ResultArea title="日本語読み" v-bind:responce="responce['ja']" />
+  <ResultArea title="中国語読み" v-bind:responce="result['zh']" />
+  <ResultArea title="日本語読み" v-bind:responce="result['ja']" />
 </div>
 </template>
 
@@ -16,6 +17,7 @@
 <script>
 import axios from 'axios'
 import ResultArea from '@/components/ResultArea.vue'
+import pinyinList from '@/assets/pinyinList.json'
 
 export default {
   name: 'ProtoScreen',
@@ -23,32 +25,95 @@ export default {
     app_name: String
   },
   data: () => ({
+    MAX_LENGTH: 8,
+    KANJI_UNKNOWN: "？",
+    KANJI_KANA_UNKNOWN: "？？",
     headerMsg: '中国人の名前を漢字かローマ字で入力してください',
     target: '',
+    errorMsg2: '文字以下で入力してください。',
     errorMsg: '漢字かローマ字を入力してください。',
-    responce: {
-      ja: null,
-      zh: null
+    result: {
+      "zh": null,
+      "ja": null
     },
   }),
   computed: {
+    lengthErrorMsg() {
+      return this.MAX_LENGTH + this.errorMsg2
+    },
     isError() {
-      return !(this.target.replace(/\s+/g, '').match(/^[\u3005-\u3006\u30e0-\u9fcf]+$/) 
-        || this.target.replace(/\s+/g, '').match(/^[A-Za-z]*$/))
+      return !(this.target.replace(/\s+/g, '').match(/^[\u3005-\u3006\u30e0-\u9fcf]+$/) ||
+        this.target.replace(/\s+/g, '').match(/^[A-Za-z]*$/))
+    },
+    isOverLimit() {
+      return this.target.length > this.MAX_LENGTH
     }
   },
   components: {
     ResultArea
   },
   methods: {
-    hoge: function() {
+    translate: function() {
+      if (this.isOverLimit) {
+        console.log("文字数で弾いた")
+        return
+      }
+      if (this.isError) {
+        console.log("文字種類で弾いた")
+        return
+      }
+
+      var syllables = this.splitBySyllable(this.target);
+
+      // 値としてコピーする
+      let syllablesString = JSON.stringify(syllables);
+      this.result["zh"] = JSON.parse(syllablesString)
+      this.result["ja"] = JSON.parse(syllablesString)
+
+
+      // すぐにわかるものは解決する
+      for (let i in this.result["ja"]) {
+        var res = this.result["ja"][i];
+        if (res["type"] == "R") {
+          res["main"] = this.KANJI_UNKNOWN;
+          res["ruby"] = this.KANJI_KANA_UNKNOWN;
+        }
+      }
+      for (let i in this.result["zh"]) {
+        var res = this.result["zh"][i];
+        if (res["type"] == "R") {
+          res["main"] = res["original"];
+          res["ruby"] = pinyinList[res["main"]]
+        }
+      }
+
+      // 既知の文字ならばここで解決したい
+
+
+      // ここまでで解決しなかったらサーバーに託す
       var url = "" + process.env.VUE_APP_SERVER_TRANSLATION;
       this.$jsonp(url, {
-        chineseName: '習近平'
+        resultString: JSON.stringify(this.result),
       }).then(data => {
-        console.log(data);
-        this.responce = data;
+        for (let i in data["zh"]) {
+          var res = data["zh"][i];
+          res["ruby"] = pinyinList[res["main"]]
+        }
+        this.result = data
       })
+    },
+    splitBySyllable: function(chineseName) {
+      let characters = chineseName.split("");
+      let syllables = [];
+      for (let i in characters) {
+        let data = {};
+        data["type"] = "K";
+        data["original"] = characters[i];
+        data["main"] = "…";
+        data["ruby"] = "…";
+        syllables.push(data);
+      }
+      return syllables;
     }
   }
 }
