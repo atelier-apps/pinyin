3970c85454e8c598bc29967afa3fe65f39277a39
koichirosawada <sawadakoichiro@gmail.com>
2019-08-17 00:16:17 -0500

    カナの全角、半角修正

--- pinyin_backend/functions/index.js
+++ pinyin_backend/functions/index.js
@@ -8,3 +8,15 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send(result);
   });
 });
+
+function getPinyinKana(pinyin){
+  //ここで引数pintinを使い、kanaを戻り値として返す
+  var pinyin_kana = $.getJSON("./pinkana.json", function(pinyin){ //外部JSONファイルの呼び出し https://www.webopixel.net/javascript/91.html
+    if (item.pinyin == pinyin) return true; //なんでtrueなんだろ https://qiita.com/1mada/items/9a48f7053a6016b5fd5a
+  }); // pinyinに対応するカナが返ってくるはず
+
+  //動作確認用のログ
+  console.log(pinyin_kana)
+
+  return pinyin_kana
+}
