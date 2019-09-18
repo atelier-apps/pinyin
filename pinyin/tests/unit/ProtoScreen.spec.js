2635df6986baaef52b516597e95d4caedf2f20bb
toshyjapan <lib_sap@yahoo.co.jp>
2019-09-19 00:28:43 +0900

    説明追加

--- pinyin/tests/unit/ProtoScreen.spec.js
+++ pinyin/tests/unit/ProtoScreen.spec.js
@@ -1,38 +1,34 @@
 import { shallowMount } from '@vue/test-utils'
 import ProtoScreen from '../../src/components/ProtoScreen.vue'
 
-// 算出プロパティの"isError"がtarget(文字列)の種類に応じて真偽を返すことをテストする
+// 算出プロパティの'isError'がtarget(文字列)の種類に応じて真偽を返すことをテストする
 test('ProtoScreen', () => {
   // コンポーネントを描画
   const wrapper = shallowMount(ProtoScreen)
 
   // `target`がひらがなのとき、エラーメッセージが表示される。
-  wrapper.setData({ target: 'ひらがな' })
+  wrapper.find('#id_search_box').setValue('ひらがな')
   expect(wrapper.find('.error-input').exists()).toBe(true)
   expect(wrapper.find('#id_charactertype').exists()).toBe(true)
-  console.log("ひらがな");
 
   // `target`がカタカナのとき、エラーメッセージが表示される。
-  wrapper.setData({ target: 'カタカナ' })
+  wrapper.find('#id_search_box').setValue('カタカナ')
   expect(wrapper.find('.error-input').exists()).toBe(true)
   expect(wrapper.find('#id_charactertype').exists()).toBe(true)
-  console.log("カタカナ");
 
-  // `target`が漢字(全角スペースあり)のとき、エラーメッセージが表示される。
-  wrapper.setData({ target: '漢　字' })
+  // `target`が漢字のとき、パスする。
+  wrapper.find('#id_search_box').setValue('漢字')
   expect(wrapper.find('.error-input').exists()).toBe(false)
   expect(wrapper.find('#id_charactertype').exists()).toBe(false)
-  console.log("漢字");
 
   // `target`が数字のとき、エラーメッセージが表示される。
-  wrapper.setData({ target: '1234' })
+  wrapper.find('#id_search_box').setValue('1234')
   expect(wrapper.find('.error-input').exists()).toBe(true)
   expect(wrapper.find('#id_charactertype').exists()).toBe(true)
-  console.log("1234");
 
-  // `target`がアルファベット(半角スペース)のとき、エラーメッセージが表示される。
-  wrapper.setData({ target: 'ka n ji' })
+  // `target`がアルファベットのとき、パスする。
+  wrapper.find('#id_search_box').setValue('nihao')
   expect(wrapper.find('.error-input').exists()).toBe(false)
   expect(wrapper.find('#id_charactertype').exists()).toBe(false)
-  console.log("ka n ji");
+
 })
