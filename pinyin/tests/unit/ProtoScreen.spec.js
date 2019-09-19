0770b64776960121947e55d515a2c2894f6ca60e
kurakurakuda <kris.3618pcp@gmail.com>
2019-09-19 21:33:53 +0900

    add unit test

--- pinyin/tests/unit/ProtoScreen.spec.js
+++ pinyin/tests/unit/ProtoScreen.spec.js
@@ -11,36 +11,82 @@ describe('ProtoScreen', () => {
 
   // `target`がひらがなのとき、エラーメッセージが表示される。
   it('Enter ひらがな in search box, then error message is displayed.', () => {
-    wrapper.find(idSearchBox).setValue('ひらがな')
-    expect(wrapper.find(classErrorInput).exists()).toBe(true)
-    expect(wrapper.find(idCharacterType).exists()).toBe(true)
+    testDiaplayErrorMsgInSearchBox('ひらがな', true)
   })
 
   // `target`がカタカナのとき、エラーメッセージが表示される。
   it('Enter カタカナ in search box, then error message is displayed.', () => {
-    wrapper.find(idSearchBox).setValue('カタカナ')
-    expect(wrapper.find(classErrorInput).exists()).toBe(true)
-    expect(wrapper.find(idCharacterType).exists()).toBe(true)
+    testDiaplayErrorMsgInSearchBox('カタカナ', true)
   })
 
   // `target`が漢字のとき、パスする。
   it('Enter 漢字 in search box, then error message is NOT displayed.', () => {
-    wrapper.find(idSearchBox).setValue('漢字')
-    expect(wrapper.find(classErrorInput).exists()).toBe(false)
-    expect(wrapper.find(idCharacterType).exists()).toBe(false)
+    testDiaplayErrorMsgInSearchBox('漢字', false)
   })
 
   // `target`が数字のとき、エラーメッセージが表示される。
   it('Enter 数字 in search box, then error message is displayed.', () => {
-    wrapper.find(idSearchBox).setValue('1234')
-    expect(wrapper.find(classErrorInput).exists()).toBe(true)
-    expect(wrapper.find(idCharacterType).exists()).toBe(true)
+    testDiaplayErrorMsgInSearchBox('0123', true)
   })
 
   // `target`がアルファベットのとき、パスする。
   it('Enter アルファベット in search box, then error message is NOT displayed.', () => {
-    wrapper.find(idSearchBox).setValue('nihao')
-    expect(wrapper.find(classErrorInput).exists()).toBe(false)
-    expect(wrapper.find(idCharacterType).exists()).toBe(false)
+    testDiaplayErrorMsgInSearchBox('nihao', false)
   })
+
+  // `target`がひらがな＋カタカナのとき、エラーメッセージが表示される。
+  it('Enter ひらがな ＋ カタカナ in search box, then error message is displayed.', () => {
+    testDiaplayErrorMsgInSearchBox('ひらカナ', true)
+  })
+
+  // `target`がひらがな＋漢字のとき、エラーメッセージが表示される。
+  it('Enter ひらがな ＋ 漢字 in search box, then error message is displayed.', () => {
+    testDiaplayErrorMsgInSearchBox('ひら字', true)
+  })
+
+  // `target`がひらがな＋数字のとき、エラーメッセージが表示される。
+  it('Enter ひらがな ＋ 数字 in search box, then error message is displayed.', () => {
+    testDiaplayErrorMsgInSearchBox('ひら0123', true)
+  })
+
+  // `target`がひらがな＋アルファベットのとき、エラーメッセージが表示される。
+  it('Enter ひらがな ＋ アルファベット in search box, then error message is displayed.', () => {
+    testDiaplayErrorMsgInSearchBox('ひらgana', true)
+  })
+
+  // `target`がカタカナ＋漢字のとき、エラーメッセージが表示される。
+  it('Enter カタカナ ＋ 漢字 in search box, then error message is displayed.', () => {
+    testDiaplayErrorMsgInSearchBox('カナ字', true)
+  })
+
+  // `target`がカタカナ＋数字のとき、エラーメッセージが表示される。
+  it('Enter カタカナ ＋ 数字 in search box, then error message is displayed.', () => {
+    testDiaplayErrorMsgInSearchBox('カナ0123', true)
+  })
+
+  // `target`がカタカナ＋アルファベットのとき、エラーメッセージが表示される。
+  it('Enter カタカナ ＋ アルファベット in search box, then error message is displayed.', () => {
+    testDiaplayErrorMsgInSearchBox('カナkana', true)
+  })
+
+  // `target`が漢字＋数字のとき、エラーメッセージが表示される。
+  it('Enter 漢字 ＋ 数字 in search box, then error message is displayed.', () => {
+    testDiaplayErrorMsgInSearchBox('漢字0123', true)
+  })
+
+  // `target`が漢字＋アルファベッットのとき、パスする。
+  it('Enter 漢字 ＋ アルファベッット in search box, then error message is NOT displayed.', () => {
+    testDiaplayErrorMsgInSearchBox('漢字kanji', false)
+  })
+
+  // `target`が数字＋アルファベッットのとき、エラーメッセージが表示される。
+  it('Enter 数字 ＋ アルファベッット in search box, then error message is displayed.', () => {
+    testDiaplayErrorMsgInSearchBox('0123kanji', true)
+  })
+
+  function testDiaplayErrorMsgInSearchBox (word, isDisplay) {
+    wrapper.find(idSearchBox).setValue(word)
+    expect(wrapper.find(classErrorInput).exists()).toBe(isDisplay)
+    expect(wrapper.find(idCharacterType).exists()).toBe(isDisplay)
+  }
 })
